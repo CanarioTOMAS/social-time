2340d1b9fa282a1d563c1ff8e099b7a6b64478e4
davidjrosso <david.rosso@gmail.com>
2023-08-01 15:57:39 -0300

    RolType

---
+++ Backed/src/resolvers/roltypes.resolvers.ts
@@ -0,0 +1,50 @@
+import { UserInputError } from "apollo-server-core";
+import RolType from "../schema/roltype";
+
+module.exports = {
+  Query: {
+    findRolType: async (_: any, _args: any, context: any) => {
+      return await RolType.find({
+        
+      });
+    },
+  },
+  Mutation: {
+    //create our mutation:
+    createRolType: async (_: any, _args: any, context: any) => {
+     
+      const roltype = new RolType({
+        name: _args.name,
+        description: _args.description
+      });
+
+      return RolType.save().catch((error: any) => {
+        throw new UserInputError(error.message, {
+          invalidArgs: _args,
+        });
+      });
+    },
+    updateRolType: async (root: any, args: any) => {
+      const { _id, ...updates } = args;
+      const roltype = await RolType.findByIdAndUpdate(_id, updates, {
+        new: true,
+      });
+      if (!roltype) {
+        throw new UserInputError("Rol Type not found", {
+          invalidArgs: args,
+        });
+      }
+      return roltype;
+    },
+    deleteRolType: async (root: any, args: any) => {
+      const { _id } = args;
+      const roltype = await RolType.findByIdAndDelete(_id);
+      if (!roltype) {
+        throw new UserInputError("Rol Type not found", {
+          invalidArgs: args,
+        });
+      }
+      return "RolType deleted successfully";
+    },
+  },
+};

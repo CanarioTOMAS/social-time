d444c6cf9279b60bdc1fd224d825f90838fa57b9
Agustin Sala <agustinsala0@gmail.com>
2023-08-03 16:20:28 -0300

    fix form register time en proceso falta funcionalidad al formulario

---
+++ frontend/src/app/pages/registerTime/page.tsx
@@ -0,0 +1,8 @@
+import FormRegesterTime from "@/app/features/register-time/components/form-registerTime";
+
+export default function RegisterTime() {
+    return (
+    <div><FormRegesterTime/></div>
+  )
+
+}

e9548258c44346b23ded82e8f10bc421523c0be1
Diego Nivoli <dnivoli@gmail.com>
2023-08-07 16:14:22 -0300

    Proyecto y registros

---
+++ Backed/src/types/record.types.ts
@@ -0,0 +1,30 @@
+import { gql } from "apollo-server-express";
+
+module.exports = gql`
+  type Record {
+    client: Client
+    user: User
+    name: String!
+    id: ID!
+  }
+
+  type Query {
+    findRecord: [Record]
+    findOneRecord(id: ID!): Record
+  }
+
+  type Mutation {
+    createRecord(
+      client: String!
+      user: String!
+      name: String!
+    ): Record
+    updateRecord(
+      _id: String!
+      client: String!
+      user: String!
+      name: String!
+    ): Record
+    deleteRecord(_id: String!): String
+  }
+`;

6f0f13d099757219a7734dc82ad2fbc25e885d56
agusala <agustinsala0@gmail.com>
2023-09-05 15:47:25 -0300

    dashboard en progreso

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,4 @@
-import 'css/tai'
+import 'tailwindcss'
 import Image from 'next/image'
 import styles from './page.module.css'
 
--- frontend/src/features/shared/components/dashboard/dashboard.tsx
+++ frontend/src/features/shared/components/dashboard/dashboard.tsx
@@ -1,35 +1,36 @@
+"use client"
 import React from "react";
 import CardPage from "./components/cards/CardPage";
 import CardSocialTraffic from "./components/cards/CardSocialTraffic";
-
-// components
-
-
+import CardBarChart from "./components/cards/CardBarChart";
+import Sidebar from "./components/sideBar/sideBar";
 
 
-const DashboardComponent: React.FC = () => {
+
+
+
+export default function DashboardComponent() {
+  
   return (
     <>
+    <Sidebar />
       <div className="flex flex-wrap">
         <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-          
+          {/* <CardPage /> */}
         </div>
         <div className="w-full xl:w-4/12 px-4">
-          
+{/* <CardBarChart/> */}
         </div>
       </div>
       <div className="flex flex-wrap mt-4">
         <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-          <CardPage />
         </div>
         <div className="w-full xl:w-4/12 px-4">
-          <CardSocialTraffic />
+          {/* <CardSocialTraffic /> */}
         </div>
       </div>
     </>
   );
-};
+}
 
 
-
-export default DashboardComponent;
